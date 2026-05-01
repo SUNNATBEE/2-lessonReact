80529abae51e4cfcb1b66806b3cd1cbc44941447
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2026-05-01 15:56:09 +0500

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
                 <strong>{randomNumber}</strong>
 
                 <h1 className='bg-red-600 text-9xl text-center'>Deploy to Vercel</h1>
+                
 
                 
                 {randomNumber % 2 === 0 ? "Juft son" : "Toq Son"}
