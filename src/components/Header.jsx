417d7e16675d910e793ee59769572be23704bc98
SUNNATBEE <yusupovsunnatbek32@gmail.com>
2026-05-01 15:21:51 +0500

    Start to deploy vercel

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+
+
+    const randomNumber = Math.round(Math.random() * 100)
+
+    return (
+        <div>
+
+           
+                <strong>{randomNumber}</strong>
+
+                <h1 className='bg-red-600 text-9xl'>Deploy to Vercel</h1>
+
+                
+                {randomNumber % 2 === 0 ? "Juft son" : "Toq Son"}
+           
+
+        </div>
+    )
+}
+
+export default Header
